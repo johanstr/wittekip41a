e1013b3e81431a8296ed0343b47d35bc218386d1
Johan Strootman <johanstr@users.noreply.github.com>
2023-04-03 11:40:58 +0200

    Resultaat van de les van 03-04-2023

--- login.php
+++ login.php
@@ -1,27 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>Inloggen | Het Gewitte Kippetje</title>
+<?php
+session_start();
 
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <nav>
-        <section class="nav-branding">
-            <img src="img/logo4.png" alt="Webshop" />
-            <h1>Witte Kip</h1>
-        </section>
-        <section class="nav-items">
-            <a href="login.php">Inloggen</a><br />
-            <a href="register.php">Registeren</a>
-        </section>
-    </nav>
-    
-    <main>
+$page_title = 'Aanmelden';
+
+@require_once('src/helpers/nav-helpers.php');
+@require_once('src/templates/bovenstukhtml.php');
+?>
+
         <form method="POST" action="src/formhandlers/login.php">
             <div>
                 <label for="email">Email</label>
@@ -33,6 +18,6 @@
             </div>
             <button type="submit">Inloggen</button>
         </form>
-    </main>
-</body>
-</html>
+    
+<?php
+@require_once('src/templates/onderstukhtml.php');
--- register.php
+++ register.php
@@ -1,27 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>Het Gewitte Kippetje</title>
+<?php
+session_start();
 
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <nav>
-        <section class="nav-branding">
-            <img src="img/logo4.png" alt="Webshop" />
-            <h1>Witte Kip</h1>
-        </section>
-        <section class="nav-items">
-            <a href="login.php">Inloggen</a><br />
-            <a href="register.php">Registeren</a>
-        </section>
-    </nav>
-    
-    <main>
+$page_title = 'Registreren';
+
+@require_once('src/helpers/nav-helpers.php');
+@require_once('src/templates/bovenstukhtml.php');
+?>
+
         <form method="POST" action="src/formhandlers/register.php">
             <div>
                 <label for="username">Gebruikersnaam</label>
@@ -37,6 +22,5 @@
             </div>
             <button type="submit">Registeer</button>
         </form>
-    </main>
-</body>
-</html>
+<?php
+@require_once('src/templates/onderstukhtml.php');
